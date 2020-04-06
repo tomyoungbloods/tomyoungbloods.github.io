351d40bf5b3e9be4b72f2abee17d3851a926c2e8
Tom Youngbloods <tomjongbloets@live.nl>
2020-04-06 16:54:36 +0200

    Basis

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyCmPsIf0osFWTMJo5CXvfnh5nYWYTkG8zY",
+    authDomain: "vue-news-70d59.firebaseapp.com",
+    databaseURL: "https://vue-news-70d59.firebaseio.com",
+    projectId: "vue-news-70d59",
+    storageBucket: "vue-news-70d59.appspot.com",
+    messagingSenderId: "531095258382",
+    appId: "1:531095258382:web:9b0a5cbf022f10100f835f",
+    measurementId: "G-ZPNLTXYGQB"
+}
